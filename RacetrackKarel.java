93757a133f4d438f56392cbbf80b024ecf1d2df7
S <s>
2012-01-11 13:05:59 -0800

    {"type":"TI", "runs":"0"}

--- RacetrackKarel.java
+++ RacetrackKarel.java
@@ -1,23 +1,5 @@
 import stanford.karel.*;
 
 public class RacetrackKarel extends Karel {
-	public void run() {
-		for (int i = 0; i < 4; i++) {
-			runOneStretch();
-		}
-	}
-	
-	/* Has Karel run one stretch of the race.
-	 * 
-	 * Precondition:  Karel is at the beginning of one stretch of
-	 *                the race.
-	 * Postcondition: Karel is at the end of that stretch and has
-	 *                turned to the left.
-	 */
-	private void runOneStretch() {
-		while (frontIsClear()) {
-			move();
-		}
-		turnLeft();
-	}
+
 }
