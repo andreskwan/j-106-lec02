15b9330b9b614027e9405d492c7f8d34c41a3e04
S <s>
2012-01-11 11:48:18 -0800

    {"type":"TI", "runs":"0"}

--- OurKarelProgram.java
+++ OurKarelProgram.java
@@ -13,6 +13,11 @@ public class OurKarelProgram extends Karel {
 		move();
 	}
 	
+	/* Causes Karel to turn to the right.
+	 *  
+	 * Precondition: None
+	 * Postcondition: Karel has turned 90 degrees to the right.
+	 */
 	private void turnRight() {
 		turnLeft();
 		turnLeft();
