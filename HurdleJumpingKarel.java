a5c02b227818d394e877b20b115b783bde433975
S <s>
2012-01-11 11:55:32 -0800

    {"type":"TI", "runs":"0"}

--- HurdleJumpingKarel.java
+++ HurdleJumpingKarel.java
@@ -1,35 +1,5 @@
 import stanford.karel.*;
 
 public class HurdleJumpingKarel extends SuperKarel {
-	public void run() {
-		for (int i = 0; i < 8; i++) {
-			if (frontIsClear()) {
-				move();
-			} else {
-				jumpHurdle();
-			}
-		}
-	}
-	
-	private void jumpHurdle() {
-		climbHurdle();
-		move();
-		descendHurdle();
-	}
-	
-	private void climbHurdle() {
-		turnLeft();
-		while (rightIsBlocked()) {
-			move();
-		}
-		turnRight();
-	}
-	
-	private void descendHurdle() {
-		turnRight();
-		while (frontIsClear()) {
-			move();
-		}
-		turnLeft();
-	}
+
 }
