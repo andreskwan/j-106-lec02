94d0f1cabbc63a8769032d2b96278bf251fb717c
S <s>
2012-01-11 15:43:49 -0800

    {"type":"TI", "runs":"0"}

--- RacetrackKarel.java
+++ RacetrackKarel.java
@@ -2,10 +2,9 @@ import stanford.karel.*;
 
 public class RacetrackKarel extends Karel {
 	public void run() {
-		runOneLength();
-		runOneLength();
-		runOneLength();
-		runOneLength();
+		for (int i = 0; i < 4; i++) {
+			runOneLength();	
+		}
 	}
 	
 	private void runOneLength() {
