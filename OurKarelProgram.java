5a2a97d6b7c5e625c6ad8265d99895d0ab380bfa
andreskwan <andreskwan@gmail.com>
2016-05-13 19:27:04 -0500

    cs106a [lec02 - OurKarelProgram.java restored]

--- OurKarelProgram.java
+++ OurKarelProgram.java
@@ -6,48 +6,14 @@ import stanford.karel.*;
  */
 public class OurKarelProgram extends SuperKarel {
 	public void run() {
-//		move();
-//		pickBeeper();
-//		move();
-//		turnLeft();
-//		move();
-//		turnRight();
-//		move();
-//		putBeeper();
-//		move();
-		moveToTheFrontPickingBeepers();
+		move();
+		pickBeeper();
+		move();
 		turnLeft();
 		move();
 		turnRight();
-		
-		//here, karel is facing a wall 
-		if(frontIsBlocked()){
-			//rotate and try to advance 
-			turnLeft();
-		}
-		//move around the wall
-	}
-	
-	private void moveToTheFrontPickingBeepers() {
-		while (frontIsClear()) {
-			if (beepersPresent()) {
-				pickBeeper();
-			}
-			move();
-		}
-	}
-	
-	/**
-	 * This method allows Karel to climb a wall that is located at his right
-	 * 
-	 * <dt><b>Precondition:</b><dd> frontIsBlocked() returns true
-	 * 
-	 * 
-	 */
-	private void climbWallAtTheRightAndTurnRight() {
-		turnLeft();
 		move();
-		//is there a wall at the east/right? 
-		
+		putBeeper();
+		move();
 	}
 }
