3bed3b51d1f371d17b083bafc566700e48e99ff8
andreskwan <andreskwan@gmail.com>
2016-05-13 19:24:53 -0500

    cs106a [lec02 - add a new world Pothole - OurKarelProgram.java restored]

--- OurKarelProgram.java
+++ OurKarelProgram.java
@@ -1,15 +1,53 @@
 import stanford.karel.*;
 
+/**
+ * @author saasbook
+ *
+ */
 public class OurKarelProgram extends SuperKarel {
 	public void run() {
-		move();
-		pickBeeper();
-		move();
+//		move();
+//		pickBeeper();
+//		move();
+//		turnLeft();
+//		move();
+//		turnRight();
+//		move();
+//		putBeeper();
+//		move();
+		moveToTheFrontPickingBeepers();
 		turnLeft();
 		move();
 		turnRight();
+		
+		//here, karel is facing a wall 
+		if(frontIsBlocked()){
+			//rotate and try to advance 
+			turnLeft();
+		}
+		//move around the wall
+	}
+	
+	private void moveToTheFrontPickingBeepers() {
+		while (frontIsClear()) {
+			if (beepersPresent()) {
+				pickBeeper();
+			}
+			move();
+		}
+	}
+	
+	/**
+	 * This method allows Karel to climb a wall that is located at his right
+	 * 
+	 * <dt><b>Precondition:</b><dd> frontIsBlocked() returns true
+	 * 
+	 * 
+	 */
+	private void climbWallAtTheRightAndTurnRight() {
+		turnLeft();
 		move();
-		putBeeper();
-		move();
+		//is there a wall at the east/right? 
+		
 	}
 }
---
+++ worlds/Pothole.w
@@ -0,0 +1,13 @@
+Dimension: (7, 4)
+Wall: (1, 1) north
+Wall: (1, 1) east
+Wall: (2, 1) east
+Wall: (4, 1) west
+Wall: (4, 1) north
+Wall: (5, 1) north
+Wall: (5, 1) east
+Wall: (6, 1) east
+Beeper: (3, 1) 1
+Karel: (1, 2) east
+BeeperBag: INFINITE
+Speed: 0.00
